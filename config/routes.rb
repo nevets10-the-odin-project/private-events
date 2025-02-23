4009c71410aa5c80c4c5c5a2d573d8fd5715a743
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-02-23 14:15:06 -0800

    Add routes and actions for attended_events

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'up' => 'rails/health#show', as: :rails_health_check
   resources :events, :users
+  resources :attended_events, only: %i[create destroy]
   root 'events#index'
 end
