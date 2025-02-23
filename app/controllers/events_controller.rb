b3f1ae6a08c2f6102f5e5c74f77f8b921b60fc64
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-02-23 11:34:17 -0800

    Add #new, #create, and auth before action to event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,14 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
+
   def index
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.build_creator(creator_number: current_user[:id])
   end
 end
