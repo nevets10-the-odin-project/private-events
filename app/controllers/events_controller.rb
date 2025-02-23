c09233e15b5f9ec46232f798882980f7038a342e
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-02-22 18:40:09 -0800

    Move associations to their respective models
    Accidentally added the association declarations to the controllers.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,4 @@
 class EventsController < ApplicationController
-  belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
-
   def index
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,4 @@
 class UsersController < ApplicationController
-  has_many :events, inverse_of: 'creator'
-
   def show
   end
 end
