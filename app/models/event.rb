9b6100caa631accb614d3493c6ab5d2d1c5994a9
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-02-23 14:36:35 -0800

    Correct through keyword

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
-  has_many :attendees, though: :attended_event, foreign_key: 'user_id', class_name: 'User'
+  has_many :attendees, through: :attended_event, foreign_key: 'user_id', class_name: 'User'
 
   validates :name, presence: true
   validates :location, presence: true
