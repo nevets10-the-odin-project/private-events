7eeab5e15911e6ce41c5ff1dd30398e24edf4a53
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-02-23 14:32:39 -0800

    Populate AttendedEvents#create, add attend button

--- app/controllers/attended_events_controller.rb
+++ app/controllers/attended_events_controller.rb
@@ -1,7 +1,21 @@
 class AttendedEventsController < ApplicationController
   def create
+    @attended_event = AttendedEvent.new(attended_event_params)
+    @attended_event.build_attendee(id: current_user[:id])
+
+    if @attended_event.save
+      redirect_to event_show_path
+    else
+      redirect_to event_show_path, status: :unprocessable_entity
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def attended_event_params
+    params.expect(attended_event: %i[event_id])
   end
 end
