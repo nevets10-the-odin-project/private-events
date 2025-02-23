f472797d710710a5553c2a5c87ed4a5da2ff1c09
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-02-23 12:59:34 -0800

    Populate events/index, add event creation logic

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,7 @@ class EventsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create]
 
   def index
+    @events = Event.all
   end
 
   def new
@@ -9,6 +10,19 @@ class EventsController < ApplicationController
   end
 
   def create
-    @event = Event.build_creator(creator_number: current_user[:id])
+    @event = Event.new(event_params)
+    @user = @event.build_creator(id: current_user[:id])
+
+    if @event.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def event_params
+    params.expect(event: %i[name location date])
   end
 end
