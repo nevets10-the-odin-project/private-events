9611b4c2cc9ab8b746649dcef37dd1609fc6e4d8
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-02-22 17:59:31 -0800

    Migrate event to db

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_22_034638) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_23_015854) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.date "event_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
