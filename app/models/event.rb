8ad8414f810cdba5df8d78a3312fa573dc6d1d37
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2025-02-23 14:08:19 -0800

    Add associates between event and user via through

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
   belongs_to :creator, foreign_key: 'user_id', class_name: 'User'
+  has_many :attendees, though: :attended_event, foreign_key: 'user_id', class_name: 'User'
+
   validates :name, presence: true
   validates :location, presence: true
   validates :event_date, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :events, inverse_of: 'creator'
+  has_many :attended_events, through: :attended_event, foreign_key: 'event_id', class_name: 'Event'
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
